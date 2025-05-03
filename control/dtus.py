c40d35f86b42a92c5b0c9b75c26a4712c62bef6a
Jörg Hedtmann <joerg.hedtmann@geomarineconsult.com>
2025-05-03 04:27:25 +0200

    Refactoring

--- control/dtus.py
+++ control/dtus.py
@@ -4,12 +4,15 @@
 This module contains the metering classes where the implementation is a true
 metering and not a control device. Subclasses that control things reside in the 'control' package.
 """
+import time
+
 from packaging import version
+from requests.auth import HTTPBasicAuth
 
 from HoymilesZeroExport import AVAILABLE, HOY_BATTERY_GOOD_VOLTAGE, session, logger, NAME, HOY_INVERTER_WATT, \
-    SERIAL_NUMBER, TEMPERATURE
+    SERIAL_NUMBER, TEMPERATURE, HOY_BATTERY_IGNORE_PANELS, HOY_PANEL_VOLTAGE_LIST, CURRENT_LIMIT
 from metering.powermeters import Powermeter
-from utils.helper_functions import cast_to_int
+from utils.helper_functions import cast_to_int, get_number_array
 
 
 class DTU(Powermeter):
@@ -56,7 +59,7 @@ class AhoyDTU(DTU):
         super().__init__(inverter_count)
         self.ip = ip
         self.password = password
-        self.Token = ''
+        self.token = ''
 
     def get_json(self, path):
         url = f'http://{self.ip}{path}'
@@ -94,60 +97,60 @@ class AhoyDTU(DTU):
             quit()
 
     def get_available(self, p_inverter_id: int):
-        ParsedData = self.get_json('/api/index')
-        Available = bool(ParsedData["inverter"][p_inverter_id]["is_avail"])
-        logger.info('Ahoy: Inverter "%s" Available: %s', NAME[p_inverter_id], Available)
-        return Available
+        parsed_data = self.get_json('/api/index')
+        available = bool(parsed_data["inverter"][p_inverter_id]["is_avail"])
+        logger.info('Ahoy: Inverter "%s" Available: %s', NAME[p_inverter_id], available)
+        return available
 
     def get_actual_limit_in_w(self, p_inverter_id: int):
-        ParsedData = self.get_json(f'/api/inverter/id/{p_inverter_id}')
-        LimitInPercent = float(ParsedData['power_limit_read'])
-        LimitInW = HOY_INVERTER_WATT[p_inverter_id] * LimitInPercent / 100
-        return LimitInW
+        parsed_data = self.get_json(f'/api/inverter/id/{p_inverter_id}')
+        limit_in_percent = float(parsed_data['power_limit_read'])
+        limit_in_w = HOY_INVERTER_WATT[p_inverter_id] * limit_in_percent / 100
+        return limit_in_w
 
     def get_info(self, p_inverter_id: int):
-        ParsedData = self.get_json('/api/live')
-        temp_index = ParsedData["ch0_fld_names"].index("Temp")
+        parsed_data = self.get_json('/api/live')
+        temp_index = parsed_data["ch0_fld_names"].index("Temp")
 
-        ParsedData = self.get_json(f'/api/inverter/id/{p_inverter_id}')
-        SERIAL_NUMBER[p_inverter_id] = str(ParsedData['serial'])
-        NAME[p_inverter_id] = str(ParsedData['name'])
-        TEMPERATURE[p_inverter_id] = str(ParsedData["ch"][0][temp_index]) + ' degC'
+        parsed_data = self.get_json(f'/api/inverter/id/{p_inverter_id}')
+        SERIAL_NUMBER[p_inverter_id] = str(parsed_data['serial'])
+        NAME[p_inverter_id] = str(parsed_data['name'])
+        TEMPERATURE[p_inverter_id] = str(parsed_data["ch"][0][temp_index]) + ' degC'
         logger.info('Ahoy: Inverter "%s" / serial number "%s" / temperature %s', NAME[p_inverter_id],
                     SERIAL_NUMBER[p_inverter_id], TEMPERATURE[p_inverter_id])
 
     def get_temperature(self, p_inverter_id: int):
-        ParsedData = self.get_json('/api/live')
-        temp_index = ParsedData["ch0_fld_names"].index("Temp")
+        parsed_data = self.get_json('/api/live')
+        temp_index = parsed_data["ch0_fld_names"].index("Temp")
 
-        ParsedData = self.get_json(f'/api/inverter/id/{p_inverter_id}')
-        TEMPERATURE[p_inverter_id] = str(ParsedData["ch"][0][temp_index]) + ' degC'
+        parsed_data = self.get_json(f'/api/inverter/id/{p_inverter_id}')
+        TEMPERATURE[p_inverter_id] = str(parsed_data["ch"][0][temp_index]) + ' degC'
         logger.info('Ahoy: Inverter "%s" temperature: %s', NAME[p_inverter_id], TEMPERATURE[p_inverter_id])
 
     def get_panel_min_voltage(self, p_inverter_id: int):
-        ParsedData = self.get_json('/api/live')
-        PanelVDC_index = ParsedData["fld_names"].index("U_DC")
+        parsed_data = self.get_json('/api/live')
+        panel_vdc_index = parsed_data["fld_names"].index("U_DC")
 
-        ParsedData = self.get_json(f'/api/inverter/id/{p_inverter_id}')
-        PanelVDC = []
-        ExcludedPanels = GetNumberArray(HOY_BATTERY_IGNORE_PANELS[p_inverter_id])
-        for i in range(1, len(ParsedData['ch']), 1):
-            if i not in ExcludedPanels:
-                PanelVDC.append(float(ParsedData['ch'][i][PanelVDC_index]))
-        minVdc = float('inf')
-        for i in range(len(PanelVDC)):
-            if (minVdc > PanelVDC[i]) and (PanelVDC[i] > 5):
-                minVdc = PanelVDC[i]
-        if minVdc == float('inf'):
-            minVdc = 0
+        parsed_data = self.get_json(f'/api/inverter/id/{p_inverter_id}')
+        panel_vdc = []
+        excluded_panels = get_number_array(HOY_BATTERY_IGNORE_PANELS[p_inverter_id])
+        for i in range(1, len(parsed_data['ch']), 1):
+            if i not in excluded_panels:
+                panel_vdc.append(float(parsed_data['ch'][i][panel_vdc_index]))
+        min_vdc = float('inf')
+        for i in range(len(panel_vdc)):
+            if (min_vdc > panel_vdc[i]) and (panel_vdc[i] > 5):
+                min_vdc = panel_vdc[i]
+        if min_vdc == float('inf'):
+            min_vdc = 0
 
         # save last 5 min-values in list and return the "highest" value.
-        HOY_PANEL_VOLTAGE_LIST[p_inverter_id].append(minVdc)
+        HOY_PANEL_VOLTAGE_LIST[p_inverter_id].append(min_vdc)
         if len(HOY_PANEL_VOLTAGE_LIST[p_inverter_id]) > 5:
             HOY_PANEL_VOLTAGE_LIST[p_inverter_id].pop(0)
         max_value = None
         for num in HOY_PANEL_VOLTAGE_LIST[p_inverter_id]:
-            if (max_value is None or num > max_value):
+            if max_value is None or num > max_value:
                 max_value = num
 
         logger.info('Lowest panel voltage inverter "%s": %s Volt', NAME[p_inverter_id], max_value)
@@ -157,6 +160,7 @@ class AhoyDTU(DTU):
         try:
             timeout = p_timeout_in_s
             timeout_start = time.time()
+            ack = False
             while time.time() < timeout_start + timeout:
                 time.sleep(0.5)
                 ParsedData = self.get_json(f'/api/inverter/id/{p_inverter_id}')
@@ -178,10 +182,10 @@ class AhoyDTU(DTU):
     def set_limit(self, p_inverter_id: int, p_limit: int):
         logger.info('Ahoy: Inverter "%s": setting new limit from %s Watt to %s Watt', NAME[p_inverter_id],
                     cast_to_int(CURRENT_LIMIT[p_inverter_id]), cast_to_int(p_limit))
-        myobj = {'cmd': 'limit_nonpersistent_absolute', 'val': p_limit, "id": p_inverter_id, "token": self.Token}
+        myobj = {'cmd': 'limit_nonpersistent_absolute', 'val': p_limit, "id": p_inverter_id, "token": self.token}
         response = self.get_response_json('/api/ctrl', myobj)
         if response["success"] == False and response["error"] == "ERR_PROTECTED":
-            self.Authenticate()
+            self.authenticate()
             self.set_limit(p_inverter_id, p_limit)
             return
         if response["success"] == False:
@@ -193,23 +197,23 @@ class AhoyDTU(DTU):
             logger.info('Ahoy: Inverter "%s": Turn on', NAME[p_inverter_id])
         else:
             logger.info('Ahoy: Inverter "%s": Turn off', NAME[p_inverter_id])
-        myobj = {'cmd': 'power', 'val': cast_to_int(p_active == True), "id": p_inverter_id, "token": self.Token}
+        myobj = {'cmd': 'power', 'val': cast_to_int(p_active == True), "id": p_inverter_id, "token": self.token}
         response = self.get_response_json('/api/ctrl', myobj)
         if response["success"] == False and response["error"] == "ERR_PROTECTED":
-            self.Authenticate()
+            self.authenticate()
             self.set_power_status(p_inverter_id, p_active)
             return
-        if response["success"] == False:
+        if not response["success"]:
             raise Exception("Error: SetPowerStatus Request error")
 
-    def Authenticate(self):
+    def authenticate(self):
         logger.info('Ahoy: Authenticating...')
         myobj = {'auth': self.password}
         response = self.get_response_json('/api/ctrl', myobj)
-        if response["success"] == False:
+        if not response["success"]:
             raise Exception("Error: Authenticate Request error")
-        self.Token = response["token"]
-        logger.info('Ahoy: Authenticating successful, received Token: %s', self.Token)
+        self.token = response["token"]
+        logger.info('Ahoy: Authenticating successful, received Token: %s', self.token)
 
 
 class OpenDTU(DTU):
@@ -219,79 +223,83 @@ class OpenDTU(DTU):
         self.user = user
         self.password = password
 
-    def GetJson(self, path):
+    def get_json(self, path):
         url = f'http://{self.ip}{path}'
         return session.get(url, auth=HTTPBasicAuth(self.user, self.password), timeout=10).json()
 
-    def GetResponseJson(self, path, sendStr):
+    def get_response_json(self, path, send_str):
         url = f'http://{self.ip}{path}'
         headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-        return session.post(url=url, headers=headers, data=sendStr, auth=HTTPBasicAuth(self.user, self.password),
+        return session.post(url=url, headers=headers, data=send_str, auth=HTTPBasicAuth(self.user, self.password),
                             timeout=10).json()
 
     def get_ac_power(self, p_inverter_id):
-        ParsedData = self.GetJson(f'/api/livedata/status?inv={SERIAL_NUMBER[p_inverter_id]}')
-        return cast_to_int(ParsedData['inverters'][0]['AC']['0']['Power']['v'])
+        parsed_data = self.get_json(f'/api/livedata/status?inv={SERIAL_NUMBER[p_inverter_id]}')
+        return cast_to_int(parsed_data['inverters'][0]['AC']['0']['Power']['v'])
 
-    def CheckMinVersion(self):
-        MinVersion = 'v24.2.12'
-        ParsedData = self.GetJson('/api/system/status')
-        OpenDTUVersion = str((ParsedData["git_hash"]))
-        if ("-Database" in OpenDTUVersion):  # trim string "v24.5.27-Database"
-            OpenDTUVersion = OpenDTUVersion.replace("-Database", "")
-        logger.info('OpenDTU: Current Version: %s', OpenDTUVersion)
-        if version.parse(OpenDTUVersion) < version.parse(MinVersion):
+    def check_min_version(self):
+        min_version = 'v24.2.12'
+        parsed_data = self.get_json('/api/system/status')
+        open_dtu_version = str((parsed_data["git_hash"]))
+        if ("-Database" in open_dtu_version):  # trim string "v24.5.27-Database"
+            open_dtu_version = open_dtu_version.replace("-Database", "")
+        logger.info('OpenDTU: Current Version: %s', open_dtu_version)
+        if version.parse(open_dtu_version) < version.parse(min_version):
             logger.error(
                 'Error: Your OpenDTU Version is too old! Please update at least to Version %s - you can find the newest dev-releases here: https://github.com/tbnobody/OpenDTU/actions',
-                MinVersion)
+                min_version)
             quit()
 
+        return
+
     def get_available(self, p_inverter_id: int):
-        ParsedData = self.GetJson(f'/api/livedata/status?inv={SERIAL_NUMBER[p_inverter_id]}')
-        Reachable = bool(ParsedData['inverters'][0]["reachable"])
-        logger.info('OpenDTU: Inverter "%s" reachable: %s', NAME[p_inverter_id], Reachable)
-        return Reachable
+        parsed_data = self.get_json(f'/api/livedata/status?inv={SERIAL_NUMBER[p_inverter_id]}')
+        reachable = bool(parsed_data['inverters'][0]["reachable"])
+        logger.info('OpenDTU: Inverter "%s" reachable: %s', NAME[p_inverter_id], reachable)
+        return reachable
 
     def get_actual_limit_in_w(self, p_inverter_id: int):
-        ParsedData = self.GetJson('/api/limit/status')
-        limit_relative = float(ParsedData[SERIAL_NUMBER[p_inverter_id]]['limit_relative'])
-        LimitInW = HOY_INVERTER_WATT[p_inverter_id] * limit_relative / 100
-        return LimitInW
+        parsed_data = self.get_json('/api/limit/status')
+        limit_relative = float(parsed_data[SERIAL_NUMBER[p_inverter_id]]['limit_relative'])
+        limit_in_w = HOY_INVERTER_WATT[p_inverter_id] * limit_relative / 100
+        return limit_in_w
 
     def get_info(self, p_inverter_id: int):
         if SERIAL_NUMBER[p_inverter_id] == '':
-            ParsedData = self.GetJson('/api/livedata/status')
-            SERIAL_NUMBER[p_inverter_id] = str(ParsedData['inverters'][p_inverter_id]['serial'])
+            parsed_data = self.get_json('/api/livedata/status')
+            SERIAL_NUMBER[p_inverter_id] = str(parsed_data['inverters'][p_inverter_id]['serial'])
 
-        ParsedData = self.GetJson(f'/api/livedata/status?inv={SERIAL_NUMBER[p_inverter_id]}')
+        parsed_data = self.get_json(f'/api/livedata/status?inv={SERIAL_NUMBER[p_inverter_id]}')
         TEMPERATURE[p_inverter_id] = str(
-            round(float((ParsedData['inverters'][0]['INV']['0']['Temperature']['v'])), 1)) + ' degC'
-        NAME[p_inverter_id] = str(ParsedData['inverters'][0]['name'])
+            round(float((parsed_data['inverters'][0]['INV']['0']['Temperature']['v'])), 1)) + ' degC'
+        NAME[p_inverter_id] = str(parsed_data['inverters'][0]['name'])
         logger.info('OpenDTU: Inverter "%s" / serial number "%s" / temperature %s', NAME[p_inverter_id],
                     SERIAL_NUMBER[p_inverter_id], TEMPERATURE[p_inverter_id])
+        return
 
     def get_temperature(self, p_inverter_id: int):
-        ParsedData = self.GetJson(f'/api/livedata/status?inv={SERIAL_NUMBER[p_inverter_id]}')
+        parsed_data = self.get_json(f'/api/livedata/status?inv={SERIAL_NUMBER[p_inverter_id]}')
         TEMPERATURE[p_inverter_id] = str(
-            round(float((ParsedData['inverters'][0]['INV']['0']['Temperature']['v'])), 1)) + ' degC'
+            round(float((parsed_data['inverters'][0]['INV']['0']['Temperature']['v'])), 1)) + ' degC'
         logger.info('OpenDTU: Inverter "%s" temperature: %s', NAME[p_inverter_id], TEMPERATURE[p_inverter_id])
+        return
 
     def get_panel_min_voltage(self, p_inverter_id: int):
-        ParsedData = self.GetJson(f'/api/livedata/status?inv={SERIAL_NUMBER[p_inverter_id]}')
-        PanelVDC = []
-        ExcludedPanels = GetNumberArray(HOY_BATTERY_IGNORE_PANELS[p_inverter_id])
-        for i in range(len(ParsedData['inverters'][0]['DC'])):
-            if i not in ExcludedPanels:
-                PanelVDC.append(float(ParsedData['inverters'][0]['DC'][str(i)]['Voltage']['v']))
-        minVdc = float('inf')
-        for i in range(len(PanelVDC)):
-            if (minVdc > PanelVDC[i]) and (PanelVDC[i] > 5):
-                minVdc = PanelVDC[i]
-        if minVdc == float('inf'):
-            minVdc = 0
+        parsed_data = self.get_json(f'/api/livedata/status?inv={SERIAL_NUMBER[p_inverter_id]}')
+        panel_vdc = []
+        excluded_panels = get_number_array(HOY_BATTERY_IGNORE_PANELS[p_inverter_id])
+        for i in range(len(parsed_data['inverters'][0]['DC'])):
+            if i not in excluded_panels:
+                panel_vdc.append(float(parsed_data['inverters'][0]['DC'][str(i)]['Voltage']['v']))
+        min_vdc = float('inf')
+        for i in range(len(panel_vdc)):
+            if (min_vdc > panel_vdc[i]) and (panel_vdc[i] > 5):
+                min_vdc = panel_vdc[i]
+        if min_vdc == float('inf'):
+            min_vdc = 0
 
         # save last 5 min-values in list and return the "highest" value.
-        HOY_PANEL_VOLTAGE_LIST[p_inverter_id].append(minVdc)
+        HOY_PANEL_VOLTAGE_LIST[p_inverter_id].append(min_vdc)
         if len(HOY_PANEL_VOLTAGE_LIST[p_inverter_id]) > 5:
             HOY_PANEL_VOLTAGE_LIST[p_inverter_id].pop(0)
         max_value = None
@@ -305,10 +313,11 @@ class OpenDTU(DTU):
         try:
             timeout = p_timeout_in_s
             timeout_start = time.time()
+            ack = False
             while time.time() < timeout_start + timeout:
                 time.sleep(0.5)
-                ParsedData = self.GetJson('/api/limit/status')
-                ack = (ParsedData[SERIAL_NUMBER[p_inverter_id]]['limit_set_status'] == 'Ok')
+                parsed_data = self.get_json('/api/limit/status')
+                ack = (parsed_data[SERIAL_NUMBER[p_inverter_id]]['limit_set_status'] == 'Ok')
                 if ack:
                     break
             if ack:
@@ -326,12 +335,13 @@ class OpenDTU(DTU):
     def set_limit(self, p_inverter_id: int, p_limit: int):
         logger.info('OpenDTU: Inverter "%s": setting new limit from %s Watt to %s Watt', NAME[p_inverter_id],
                     cast_to_int(CURRENT_LIMIT[p_inverter_id]), cast_to_int(p_limit))
-        relLimit = cast_to_int(p_limit / HOY_INVERTER_WATT[p_inverter_id] * 100)
-        mySendStr = f'''data={{"serial":"{SERIAL_NUMBER[p_inverter_id]}", "limit_type":1, "limit_value":{relLimit}}}'''
-        response = self.GetResponseJson('/api/limit/config', mySendStr)
+        rel_limit = cast_to_int(p_limit / HOY_INVERTER_WATT[p_inverter_id] * 100)
+        my_send_str = f'''data={{"serial":"{SERIAL_NUMBER[p_inverter_id]}", "limit_type":1, "limit_value":{rel_limit}}}'''
+        response = self.get_response_json('/api/limit/config', my_send_str)
         if response['type'] != 'success':
             raise Exception(f"Error: SetLimit error: {response['message']}")
         CURRENT_LIMIT[p_inverter_id] = p_limit
+        return
 
     def set_power_status(self, p_inverter_id: int, p_active: bool):
         if p_active:
@@ -340,9 +350,9 @@ class OpenDTU(DTU):
             logger.info('OpenDTU: Inverter "%s": Turn off', NAME[p_inverter_id])
 
         logger.debug(f'POWER: {p_active}')
-        mySendStr = f'''data={{"serial":"{SERIAL_NUMBER[p_inverter_id]}", "power":{str(p_active).lower()}}}'''
-        logger.info(mySendStr)
-        response = self.GetResponseJson('/api/power/config', mySendStr)
+        my_send_str = f'''data={{"serial":"{SERIAL_NUMBER[p_inverter_id]}", "power":{str(p_active).lower()}}}'''
+        logger.info(my_send_str)
+        response = self.get_response_json('/api/power/config', my_send_str)
         if response['type'] != 'success':
             raise Exception(f"Error: SetPowerStatus error: {response['message']}")
 
@@ -354,7 +364,7 @@ class DebugDTU(DTU):
     def get_ac_power(self, p_inverter_id):
         return cast_to_int(input("Current AC-Power: "))
 
-    def CheckMinVersion(self):
+    def check_min_version(self):
         return
 
     def get_available(self, p_inverter_id: int):
